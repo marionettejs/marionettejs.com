de3d5532cdd8ee4ceedfcbd02996b8c3873ac357
Sam Saccone <samccone@gmail.com>
2014-09-23 19:20:45 -0400

    update deploy repo target

--- ship.sh
+++ ship.sh
@@ -3,7 +3,7 @@ cd dist
 git init
 git add .
 git commit -m "deploy"
-git remote add origin git@github.com:thejameskyle/marionettejs.com.git
+git remote add origin git@github.com:marionettejs/marionettejs.com.git
 git push -f origin master:gh-pages
 rm -rf .git
 cd ..
