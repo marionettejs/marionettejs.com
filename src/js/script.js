38b111210f71ad45bc2154afe342664049cbc114
Joanne Daudier <jdaudier@outlook.com>
2014-10-11 01:21:22 -0400

    Add back to top button

--- src/js/script.js
+++ src/js/script.js
@@ -37,4 +37,27 @@ $(document).ready(function() {
       var txt = $this.text() === 'more'? 'less': 'more';
       $this.text(txt);
     });
+
+    // Back to top button
+    var offset = 300; //"back to top" button is shown
+    var offset_opacity = 1200; //"back to top" button opacity is reduced
+    var scroll_top_duration = 700; //duration of the top scrolling animation (in ms)
+    var $backToTopBtn = $('.top');
+
+    //hide or show the "back to top" link
+    $(window).scroll(function(){
+        ($(this).scrollTop() > offset) ? $backToTopBtn.addClass('is-visible') : $backToTopBtn.removeClass('is-visible fade-out');
+        if($(this).scrollTop() > offset_opacity) {
+            $backToTopBtn.addClass('fade-out');
+        }
+    });
+
+    //smooth scroll to top
+    $backToTopBtn.on('click', function(e){
+        e.preventDefault();
+        $('body, html').animate({
+              scrollTop: 0
+          }, scroll_top_duration
+        );
+    });
 });
