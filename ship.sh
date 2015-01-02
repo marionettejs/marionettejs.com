8e711085b540fff0a3b45f8c7ca8cb327f0eb851
Sam Saccone <samccone@gmail.com>
2015-01-02 18:41:10 -0500

    Tweak ship file to fail if any step bombs

--- ship.sh
+++ ship.sh
@@ -1,10 +1,13 @@
-npm run compile-all
+#!/bin/bash
+set -o pipefail
+
+npm run compile-all && \
 cp CNAME dist
-cd dist
-git init
-git add .
-git commit -m "deploy"
-git remote add origin git@github.com:marionettejs/marionettejs.com.git
-git push -f origin master:gh-pages
-rm -rf .git
+cd dist && \
+git init && \
+git add . && \
+git commit -m "deploy" && \
+git remote add origin git@github.com:marionettejs/marionettejs.com.git && \
+git push -f origin master:gh-pages && \
+rm -rf .git && \
 cd ..
