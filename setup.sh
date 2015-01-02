fc948e7110fb35c62ba93ca97de58568ea9cd46f
Sam Saccone <samccone@gmail.com>
2015-01-02 18:44:55 -0500

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
-npm install
-bower install
+#!/bin/bash
+set -o pipefail
+
+npm i && \
 git clone git@github.com:marionettejs/backbone.marionette.git
-cd backbone.marionette
-bower install
-npm install
