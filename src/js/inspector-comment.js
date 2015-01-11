78b3fd793b21a75ace944a801159acba7c54b041
Graeme <megawac@gmail.com>
2015-01-11 15:49:27 -0500

    Log inspector notification to console

---
+++ src/js/inspector-comment.js
@@ -0,0 +1,13 @@
+// Give a notice about how awesome inspector is in console
+var msg = [
+    '            .---._-------------------------------------+',
+    '___________/ ._____)                                   |',
+    '              ) __|   HEY HACKER! MARIONETTE IS        |',
+    '                __|   EVEN COOLER WHEN YOU USE THE     |',
+    '..---------.._____|   INSPECTOR. http://goo.gl/Wo3pju  |',
+    '                  +------------------------------------+'
+].join('\n');
+
+try {
+    console.log('%c' + msg, 'background: #A80000; color: #EBF2C2');
+} catch(o_O) {}
