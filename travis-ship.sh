029ff33c609efd3bbef38e078bab17a5ea34d23b
Sam Saccone <samccone@gmail.com>
2015-02-13 15:20:46 -0500

    d

--- travis-ship.sh
+++ travis-ship.sh
@@ -5,4 +5,4 @@ touch foo.txt;
 git add .;
 git commit -m "wow";
 
-git push --force --quiet "https://${GH_TOKEN}@$github.com/marionettejs/marionettejs.com.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@github.com/marionettejs/marionettejs.com.git" master:foo > /dev/null 2>&1
