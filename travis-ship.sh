6ce8e9ae41013f754ec08eae792e33e878ac8d91
Sam Saccone <samccone@gmail.com>
2015-02-13 15:20:09 -0500

    cry

--- travis-ship.sh
+++ travis-ship.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -o pipefail
 
-bower i && \
-npm run compile-all && \
-cp CNAME dist
-cd dist && \
-git init && \
-git add . && \
-git commit -m "deploy" && \
+touch foo.txt;
+git add .;
+git commit -m "wow";
+
 git push --force --quiet "https://${GH_TOKEN}@$github.com/marionettejs/marionettejs.com.git" master:gh-pages > /dev/null 2>&1
