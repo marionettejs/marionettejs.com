38c452b10c17fcd114601e602602ed7c9b63348a
Sam Saccone <samccone@gmail.com>
2015-02-13 15:25:57 -0500

    fizz

--- travis-ship.sh
+++ travis-ship.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -o pipefail
 
-touch foo.txt;
-git add .;
-git commit -m "wow";
-
-git push --force --quiet "https://${GH_TOKEN}@github.com/marionettejs/marionettejs.com.git" master:foo > /dev/null 2>&1
+bower i && \
+npm run compile-all && \
+cp CNAME dist
+cd dist && \
+git init && \
+git add . && \
+git commit -m "deploy" && \
+git push --force --quiet "https://${GH_TOKEN}@github.com/marionettejs/marionettejs.com.git" master:gh-pages > /dev/null 2>&1
