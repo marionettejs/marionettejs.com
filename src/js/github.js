79533e00ef8f3219411118ce2d21e074155a681b
Trezy <tre@trezy.com>
2015-10-26 11:41:24 -0500

    [#406] Add Marionette Team section

--- src/js/github.js
+++ src/js/github.js
@@ -1,5 +1,6 @@
 ;(function($) {
   var url = 'https://api.github.com/repos/marionettejs/backbone.marionette?callback=?';
+
   $.getJSON(url)
   .done(function(result) {
     if (result.meta.status === 200) {
@@ -7,6 +8,41 @@
       /*jshint camelcase: false */
       var stargazers = result.data.stargazers_count || '5700';
       $('.stargazers-js').text(stargazers);
+    }
+  });
+})(jQuery);
+
+(function($) {
+  var url = 'https://api.github.com/orgs/marionettejs/members';
+
+  //jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+  /*jshint camelcase: false */
+  $.getJSON(url, {
+    client_id: '0ab3cd67b835df549748',
+    client_secret: '6a253f50df77585a5aff0a61046a7f4fac5ea134'
+  })
+  .done(function(members) {
+    //jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+    /*jshint camelcase: false */
+    var i;
+    var memberCount = members.length;
+    var memberListEl = $('.member-list');
+
+    for (i = 0; i < memberCount; i++) {
+      var member = members[i];
+      var listItemEl = $(document.createElement('li'));
+      var memberEl = $(document.createElement('a'));
+
+      listItemEl.addClass('col-4');
+
+      memberEl
+        .addClass('team-member')
+        .attr('href', member.html_url)
+        .attr('data-username', member.login)
+        .css('background-image', 'url(' + member.avatar_url + '&s=256)');
+
+      listItemEl.append(memberEl);
+      memberListEl.append(listItemEl);
     }
   });
 })(jQuery);
