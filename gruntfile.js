8476f6c9de440c1c5c14acdfdacf9ea118a970fc
Artem Denysov <denysov.artem@gmail.com>
2016-05-12 14:06:59 +0300

    fix jshint error

--- gruntfile.js
+++ gruntfile.js
@@ -62,7 +62,9 @@ module.exports = function(grunt) {
     compileAdditionalResources: {
       marionette: {
         options: {
+          /*jshint ignore:start*/
           resUrl: 'https://raw.githubusercontent.com/sadcitizen/awesome-marionette/master/README.md',
+          /*jshint ignore:end*/
           output: 'dist/data'
         }
       }
