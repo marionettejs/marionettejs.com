12bbb4dab4d1bfa7557f5514ade7a133afcd0f15
Artem Denysov <denysov.artem@gmail.com>
2017-05-10 23:54:07 +0300

    push in other manner (#504)

--- travis-runner.sh
+++ travis-runner.sh
@@ -13,7 +13,7 @@ then
   git init
   git add .
   git commit -m "deploy"
-  git push --force --quiet "https://${GH_TOKEN}@github.com/marionettejs/marionettejs.com.git" master:gh-pages > /dev/null 2>&1
+  git push --force "https://${GH_TOKEN}@github.com/marionettejs/marionettejs.com.git" gh-pages
 else
   npm run lint && npm run pagespeed
 fi
