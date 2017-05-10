71ce615e30d9067d080a3309e08eb7960f9a019f
Artem Denysov <denysov.artem@gmail.com>
2017-05-10 23:46:10 +0300

    Update travis-runner.sh (#502)

--- travis-runner.sh
+++ travis-runner.sh
@@ -13,7 +13,6 @@ then
   git init
   git add .
   git commit -m "deploy"
-  git push --force --quiet "https://${GH_TOKEN}@github.com/marionettejs/marionettejs.com.git" master:gh-pages > /dev/null 2>&1
 else
   npm run lint && npm run pagespeed
 fi
